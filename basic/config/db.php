f72649b973f51bade5f6f33ec3263cb7961e7482
yan <130693355@qq.com>
2016-08-03 18:40:19 +0800

    d

--- basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-					return [
-						'class' => 'yii\db\Connection',
-						'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=yan',
-						'username' => 'root',
-						'password' => 'root',
-						'charset' => 'utf8',
-						'tablePrefix' => 'we_',   //加入前缀名称we_
-					];
--- basic/web/assets/abc.php
+++
@@ -1,3 +0,0 @@
-<?php
-                $pdo=new PDO('mysql:host=127.0.0.1;port=3306;dbname=yan','root','root',array(PDO::MYSQL_ATTR_INIT_COMMAND=>'set names utf8'));
-                   ?>
